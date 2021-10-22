74f9e40963c6d4e92f9128e5ffbea1f8eb990d9d
Juan Camilo Garcia <jcgm1047@gmail.com>
2021-10-21 21:15:29 -0500

    configuracion bd

--- views/auth/registroUsuario.php
+++ views/auth/registroUsuario.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Document</title>
 </head>
 <body>
@@ -16,6 +17,6 @@
     <label for="">telefono</label>
     <input type="tel" name="telefono" id="telefono">
     <label for="">Contraseña</label>
-    <input type="password" name="contraseña" id="contraseña">
+    <input type="password" name="contrasena" id="contrasena">
 </body>
 </html>
