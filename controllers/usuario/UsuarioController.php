78026e6b9a0c78e1e79940179dcabc505bae82b6
Juan Camilo Garcia <jcgm1047@gmail.com>
2021-10-21 22:22:42 -0500

    ultimos cambios

--- controllers/usuario/UsuarioController.php
+++ controllers/usuario/UsuarioController.php
@@ -1,3 +1,10 @@
 <?php 
 include './config/dbconection.php';
+$nombre = $_POST["nombre"];
+$apellido = $_POST["apellido"];
+
+$query =  "INSERT INTO registro_usuario (nombre, apellido, email, tel, contrasena) VALUES ('$nombre', '$apellido')";
+$mysqli->query($query);
+
+
 ?>
