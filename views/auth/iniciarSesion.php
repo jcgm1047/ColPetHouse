028833461ad25fffe38bc3a17afd81fb16fc30e9
Juan Camilo Garcia <jcgm1047@gmail.com>
2021-10-21 21:07:36 -0500

    PRoyecto colpethouse

---
+++ views/auth/iniciarSesion.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Iniciar Sesion</h1>
+
+    <label for="">Nombre Usuario</label>
+    <input type="text" name="user" id="user">
+    <label for="">Contraseña</label>
+    <input type="text" name="password" id="password">
+</body>
+</html>
---
+++ views/auth/registroUsuario.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <label for="">Nombre Usuario</label>
+    <input type="text" name="nombre" id="nombre">
+    <label for="">Apellido Usuario</label>
+    <input type="text" name="apellido" id="apellido">
+    <label for="">Correo</label>
+    <input type="email" name="email" id="email">
+    <label for="">telefono</label>
+    <input type="tel" name="telefono" id="telefono">
+    <label for="">Contraseña</label>
+    <input type="password" name="contraseña" id="contraseña">
+</body>
+</html>
